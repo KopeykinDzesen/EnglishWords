bf3a25ac3872047c4d6ac3600690312697a12b30
dzesen <kopeykin.dzesen2MAIL.RU>
2018-01-07 01:26:18 +0300

    Initial commit

---
+++ generator_of_words.py
@@ -0,0 +1,75 @@
+import random
+
+dict_of_words = {}
+count_of_chance = 0
+dict_of_chance = {}
+word_history = []
+answer_history = []
+
+
+def get_random_word():
+    random_word = ""
+    random_number = random.randrange(1, count_of_chance)
+    sum_of_chance = 0
+    for i in dict_of_chance:
+        sum_of_chance += int(dict_of_chance[i])
+        if sum_of_chance >= random_number:
+            random_word = i
+            break
+    return random_word
+
+
+def fill_words():
+    global dict_of_words
+    dictionary = open("dictionary", "r")
+    data = dictionary.readlines()
+    for i in data:
+        english_word, russian_word = i.split(":")
+        dict_of_words[english_word] = russian_word.strip()
+        dict_of_words[russian_word.strip()] = english_word
+
+
+def fill_chance():
+    global dict_of_chance, count_of_chance
+    dictionary = open("dict_chance", "r")
+    data = dictionary.readlines()
+    for i in data:
+        word, chance = i.split(":")
+        dict_of_chance[word] = chance.strip()
+    for i in dict_of_words.keys():
+        if i not in dict_of_chance.keys():
+            dict_of_chance[i] = 1
+    for i in dict_of_chance.values():
+        count_of_chance += int(i)
+
+
+def save_chance():
+    file = open("dict_chance", "w")
+    for i in dict_of_chance:
+        file.write("{}:{}\n".format(i, dict_of_chance[i]))
+
+
+def fill_history():
+    global word_history, answer_history
+    history = open("history", "r")
+    data = history.readline().split("_")
+    for i in data:
+        try:
+            words, answer = i.split(":")
+        except ValueError:
+            break
+        word_history.append(words)
+        answer_history.append(answer)
+
+
+def save_history():
+    global word_history, answer_history
+    history = open("history", "w")
+    print(word_history)
+    print(answer_history)
+    lenght = len(word_history)
+    if lenght > 50:
+        word_history = word_history[lenght-50:]
+        answer_history = answer_history[lenght-50:]
+    for i in range(lenght):
+        history.write("{}:{}_".format(word_history[i], str(answer_history[i])))
---
+++ main.py
@@ -0,0 +1,78 @@
+from tkinter import *
+import generator_of_words as gen
+
+
+def check_word():
+    word = label["text"]
+    translate_word = entry.get()
+    result = (gen.dict_of_words[word] == translate_word)
+    if result:
+        label.configure(fg="green")
+        label["text"] = "{} - {}".format(word, gen.dict_of_words[word])
+        gen.word_history.append(label["text"])
+        gen.answer_history.append("True")
+        if gen.dict_of_chance[word] > 1:
+            gen.dict_of_chance[word] = int(gen.dict_of_chance[word]) - 1
+            gen.count_of_chance = int(gen.count_of_chance) - 1
+    else:
+        label.configure(fg="red")
+        label["text"] = "{} - {}".format(word, gen.dict_of_words[word])
+        gen.word_history.append(label["text"])
+        gen.answer_history.append("False")
+        gen.dict_of_chance[word] = int(gen.dict_of_chance[word]) + 1
+        gen.count_of_chance = int(gen.count_of_chance) + 1
+    btn_check.grid_forget()
+    btn_next.grid(row=2, column=3)
+
+
+def show_history():
+    window_history = Toplevel(root)
+    window_history.title("This is history")
+    window_history.minsize(600, 600)
+    window_history.maxsize(600, 600)
+    history = Text(window_history, width=40, height=50, font=("Ubuntu", 15))
+    history.grid(row=1, column=1)
+    history.delete("1.0", END)
+    length = len(gen.word_history)
+    for i in range(length):
+        stroka = "{} : {}\n".format(gen.word_history[length-i-1], gen.answer_history[length-i-1])
+        history.insert(END, stroka)
+
+
+def next_word():
+    label.configure(text=gen.get_random_word(), fg="black")
+    btn_next.grid_forget()
+    btn_check.grid(row=2, column=3)
+
+
+def end_app():
+    gen.save_chance()
+    gen.save_history()
+    root.destroy()
+
+
+root = Tk()
+root.title("Translate it, please.")
+root.minsize(1185, 175)
+root.maxsize(1185, 175)
+root.protocol("WM_DELETE_WINDOW", end_app)
+
+label = Label(root, width=20, font=("Ubuntu", 50))
+label.grid(row=1, columnspan=2)
+
+entry = Entry(root, width=32, font=("Ubuntu", 30))
+entry.grid(row=2, columnspan=2)
+
+btn_check = Button(root, text="check", font=("Ubuntu", 30), command=check_word)
+btn_show_history = Button(root, text="history", font=("Ubuntu", 30), command=show_history)
+btn_next = Button(root, text="next", font=("Ubuntu", 30), command=next_word)
+btn_next.grid(row=2, column=3)
+btn_show_history.grid(row=1, column=3)
+
+gen.fill_words()
+gen.fill_chance()
+gen.fill_history()
+
+root.mainloop()
+
+
